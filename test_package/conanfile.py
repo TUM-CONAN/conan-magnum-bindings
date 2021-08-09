c19a867438b7d21f8932b0db0eb4626d05d9eb95
Ulrich Eck <ulrich.eck@tum.de>
2021-08-09 22:37:41 +0200

    fix test for system python

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -6,4 +6,16 @@ class MagnumTestConan(ConanFile):
 
     def test(self):
       # self.conanfile_directory
-      self.run('{0} -c "from magnum import Vector2; print(Vector2(1.0, 2.0))"'.format(os.environ.get("PYTHON")))
+
+      # some sensible defaults            
+      python = None
+      if "PYTHON" in os.environ:
+          python = os.environ.get("PYTHON")
+
+      if python is None:
+          python = self.deps_env_info["python_dev_config"].PYTHON
+
+      if not python:
+          python = self.options["python_dev_config"].python
+
+      self.run('{0} -c "from magnum import Vector2; print(Vector2(1.0, 2.0))"'.format(python))
